b20884e7c34ee37378513276c33ef29335ef9731
Lovish Duggal <104680808+lovishduggal@users.noreply.github.com>
2022-07-17 23:40:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const themeSelector = document.querySelector("#themeSelector");
+
+navigator.geolocation.getCurrentPosition((position) => {
+  console.log(position);
+  let sunset = new Date().sunset(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+  let sunrise = new Date().sunrise(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+
+  //this gave us india sunrise and sunset time :
+  console.log(sunrise.getHours(), sunset.getHours());
+
+  if (isDay(sunset, sunrise)) {
+    setTheme("colorLight");
+  } else {
+    setTheme("colorDark");
+  }
+
+  // const nowHours = new Date().getHours()--> this gave me present time
+  //     console.log( nowHours )
+
+  function isDay(sunset, sunrise) {
+    const nowHours = new Date().getHours();
+    return nowHours >= sunrise.getHours() && nowHours < sunset.getHours();
+  }
+});
+function defColor() {
+  document.documentElement.className =
+    localStorage.getItem("theme") || "colorLight";
+  themeSelector.value = localStorage.getItem("theme");
+}
+defColor();
+themeSelector.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  setColor(e.target.value);
+});
+function setColor(color) {
+  console.log(themeSelector.value);
+  themeSelector.value = color;
+  document.documentElement.className = color;
+
+  localStorage.setItem("theme", color);
+}
